55175a60ddc0cd9a1c5d460e337330b7cb9f2e82
Ovidiu Cretu <cretuovidiudaniel@yahoo.com>
2020-03-14 22:46:31 +0200

    Ex5Done

--- src/main/java/isp/lab3/exercise5/Exercise5.java
+++ src/main/java/isp/lab3/exercise5/Exercise5.java
@@ -1,4 +1,36 @@
 package isp.lab3.exercise5;
 
+import java.util.Scanner;
+
 public class Exercise5 {
+    public static void main(String[] args) {
+        VendingMachine V1 = new VendingMachine(5);
+        V1.displayProducts();
+        System.out.println("Hello! Please introduce the 4 digit code to access the menu or press 0 to buy a product.");
+        Scanner in = new Scanner(System.in);
+        int code = in.nextInt();
+        int numberOfTimesForWrongCode = 0;
+        if (code == 0) {
+            V1.userMenu();
+        } else {
+            if (code == 2007) {
+                V1.adminMenu();
+            } else {
+                {
+                    while (numberOfTimesForWrongCode < 4 && code != 2007) {
+                        System.out.println("Wrong code! Try again!");
+                        code = in.nextInt();
+                        numberOfTimesForWrongCode++;
+                    }
+                    if (numberOfTimesForWrongCode == 4)
+                    System.out.println("Vending Machine out of usage");
+                    else{
+                        V1.adminMenu();
+                    }
+                }
+            }
+        }
+    }
 }
+
+
---
+++ src/test/java/isp/lab3/exercise5/VendingMachineTest.java
@@ -0,0 +1,22 @@
+package isp.lab3.exercise5;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class VendingMachineTest {
+    VendingMachine V2 = new VendingMachine(5);
+    @Test
+    public void displayProducts(){
+        String testproduct="";
+       /// testproduct=V2.displayProducts();
+       /// assertEquals("Product Empty have id 0 ,price 0 and there are 0 of this product","Product Empty have id 1 ,price 0 and there are 0 of this product",testproduct);
+    }
+    @Test
+    public void insertCoinTest(){
+        assertEquals("The credit should ne 10",10,V2.insertCoin(10));
+    }
+    @Test
+    public void selectProductTest(){
+        assertEquals("Should be Empty","Empty",V2.selectProduct(0));
+    }
+}
